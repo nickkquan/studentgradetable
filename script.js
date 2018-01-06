2e13e900cde7bd4a006629fa15a1ebe83be0e201
Nick Quan <nickkquan@gmail.com>
2018-01-06 14:10:11 -0800

    v0.5 - JS functionality added. Add Student/Clear Student and DOM elements functioning.

--- script.js
+++ script.js
@@ -5,46 +5,47 @@
 /**
  * Listen for the document to load and initialize the application
  */
-$(document).ready();
-
+$(document).ready(initializeApp);
 /**
- * Define all global variables here.  
+ * Define all global variables here.
  */
 /***********************
  * student_array - global array to hold student objects
  * @type {Array}
- * example of student_array after input: 
+ * example of student_array after input:
  * student_array = [
  *  { name: 'Jake', course: 'Math', grade: 85 },
  *  { name: 'Jill', course: 'Comp Sci', grade: 85 }
  * ];
  */
-
+var student_array = [];
+var counter = 0;
 /***************************************************************************************************
-* initializeApp 
-* @params {undefined} none
-* @returns: {undefined} none
-* initializes the application, including adding click handlers and pulling in any data from the server, in later versions
-*/
-function initializeApp(){
+ * initializeApp
+ * @params {undefined} none
+ * @returns: {undefined} none
+ * initializes the application, including adding click handlers and pulling in any data from the server, in later versions
+ */
+function initializeApp() {
+  addClickHandlersToElements();
 }
-
 /***************************************************************************************************
-* addClickHandlerstoElements
-* @params {undefined} 
-* @returns  {undefined}
-*     
-*/
-function addClickHandlersToElements(){
+ * addClickHandlerstoElements
+ * @params {undefined}
+ * @returns  {undefined}
+ */
+function addClickHandlersToElements() {
+  $(".add").on("click", handleAddClicked);
+  $(".cancel").on("click", handleCancelClick);
 }
-
 /***************************************************************************************************
  * handleAddClicked - Event Handler when user clicks the add button
  * @param {object} event  The event object from the click
  * @return: 
        none
  */
-function handleAddClicked(){
+function handleAddClicked() {
+  addStudent();
 }
 /***************************************************************************************************
  * handleCancelClicked - Event Handler when user clicks the cancel button, should clear out student form
@@ -52,7 +53,8 @@ function handleAddClicked(){
  * @returns: {undefined} none
  * @calls: clearAddStudentFormInputs
  */
-function handleCancelClick(){
+function handleCancelClick() {
+  clearAddStudentFormInputs();
 }
 /***************************************************************************************************
  * addStudent - creates a student objects based on input fields in the form and adds the object to global student array
@@ -60,46 +62,75 @@ function handleCancelClick(){
  * @return undefined
  * @calls clearAddStudentFormInputs, updateStudentList
  */
-function addStudent(){
+function addStudent() {
+  var studentObject = {};
+
+  studentObject.name = $("#studentName").val();
+  studentObject.course = $("#course").val();
+  studentObject.grade = parseInt($("#studentGrade").val());
+
+  student_array.push(studentObject);
+  updateStudentList(student_array);
+  clearAddStudentFormInputs();
 }
 /***************************************************************************************************
  * clearAddStudentForm - clears out the form values based on inputIds variable
  */
-function clearAddStudentFormInputs(){
+function clearAddStudentFormInputs() {
+  $("#studentName").val("");
+  $("#course").val("");
+  $("#studentGrade").val("");
 }
 /***************************************************************************************************
  * renderStudentOnDom - take in a student object, create html elements from the values and then append the elements
  * into the .student_list tbody
  * @param {object} studentObj a single student object with course, name, and grade inside
  */
-function renderStudentOnDom(){
-}
+function renderStudentOnDom(object) {
+  var newRow = $("<tr>");
+  var deleteButton = $("<button>")
+    .attr("type", "button")
+    .addClass("btn btn-danger btn-sm")
+    .text("Delete");
+  var name = $("<td>").text(object.name);
+  var course = $("<td>").text(object.course);
+  var grade = $("<td>").text(object.grade);
+  var button = $("<td>").append(deleteButton);
 
+  newRow.append(name, course, grade, button);
+  $("tbody").append(newRow);
+}
 /***************************************************************************************************
  * updateStudentList - centralized function to update the average and call student list update
  * @param students {array} the array of student objects
  * @returns {undefined} none
  * @calls renderStudentOnDom, calculateGradeAverage, renderGradeAverage
  */
-function updateStudentList(){
-  
+function updateStudentList(array) {
+  for (var arrayIndex = counter; arrayIndex < array.length; arrayIndex++) {
+    renderStudentOnDom(array[arrayIndex]);
+    counter++;
+  }
+  renderGradeAverage(calculateGradeAverage(array));
 }
 /***************************************************************************************************
  * calculateGradeAverage - loop through the global student array and calculate average grade and return that value
- * @param: {array} students  the array of student objects
+ * @param: {array} students the array of student objects
  * @returns {number}
  */
-function calculateGradeAverage(){
+function calculateGradeAverage(array) {
+  var number = 0;
+  for (var studentIndex = 0; studentIndex < array.length; studentIndex++) {
+    number += array[studentIndex].grade;
+  }
+  var averageGrade = number / array.length;
+  return averageGrade;
 }
 /***************************************************************************************************
  * renderGradeAverage - updates the on-page grade average
  * @param: {number} average    the grade average
  * @returns {undefined} none
  */
-function renderGradeAverage(){
+function renderGradeAverage(number) {
+  $(".avgGrade").text(number);
 }
-
-
-
-
-
